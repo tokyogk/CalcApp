e2fd7e8ce1680e9ff63a2df44e0706fae6cc82d0
tokyogk <tokyo.gk5@gmail.com>
2017-09-28 17:57:31 +0900

    修正完了。課題提出用。

--- app/src/main/java/jp/techacademy/gou/kuwabara/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/gou/kuwabara/calcapp/MainActivity.java
@@ -1,13 +1,10 @@
 package jp.techacademy.gou.kuwabara.calcapp;
 
 import android.content.Intent;
-import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -41,44 +38,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // 変換に失敗した場合に、メッセージを出すなど
         }
 
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra("editText1", num1);
-        intent.putExtra("editText2", num2);
-        intent.putExtra("textView", num3);
-        startActivity(intent);
-
         if (v != null) {
             switch (v.getId()) {
                 case R.id.button1:
-                    TextView textView = (TextView) findViewById(R.id.textView);
                     num3 = num1 + num2;
-                    textView.setText(String.valueOf(num3));
                     // クリック処理
                     break;
 
                 case R.id.button2:
-                    textView = (TextView) findViewById(R.id.textView);
                     num3 = num1 - num2;
-                    textView.setText(String.valueOf(num3));
                     // クリック処理
                     break;
 
                 case R.id.button3:
-                    textView = (TextView) findViewById(R.id.textView);
                     num3 = num1 * num2;
-                    textView.setText(String.valueOf(num3));
                     // クリック処理
                     break;
 
                 case R.id.button4:
-                    textView = (TextView) findViewById(R.id.textView);
                     num3 = num1 / num2;
-                    textView.setText(String.valueOf(num3));
                     // クリック処理
                     break;
             }
         }
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("editText1", num1);
+        intent.putExtra("editText2", num2);
+        intent.putExtra("textView1", num3);
+        startActivity(intent);
     }
-
-
 }
--- app/src/main/java/jp/techacademy/gou/kuwabara/calcapp/SecondActivity.java
+++ app/src/main/java/jp/techacademy/gou/kuwabara/calcapp/SecondActivity.java
@@ -1,12 +1,8 @@
 package jp.techacademy.gou.kuwabara.calcapp;
 
 import android.content.Intent;
-import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -17,11 +13,9 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         Intent intent = getIntent();
-        Double editText1 = intent.getDoubleExtra("editText1", 0);
-        Double editText2 = intent.getDoubleExtra("editText2", 0);
-        Double textView1 = intent.getDoubleExtra("textView", 0);
+        Double textView1 = intent.getDoubleExtra("textView1", 0);
 
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(String.valueOf(textView));
+        TextView textView = (TextView) findViewById(R.id.textView1);
+        textView.setText(String.valueOf(textView1));
     }
 }
